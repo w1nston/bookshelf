da01529fec0ef2bdce4cc1b33fc309ee73e08255
w1nston <wallander.jonas@gmail.com>
2016-06-08 21:48:15 +0200

    Fix tests for the actions. Remove unused constant.

--- app/actions/__tests__/booksActions.spec.js
+++ app/actions/__tests__/booksActions.spec.js
@@ -1,11 +1,53 @@
 import expect from 'expect';
-import { clearBookForm } from '../booksActions';
+import * as actions  from '../booksActions';
 import * as types from '../../constants';
 
-describe('clearBookForm', () => {
-  it('returns an object with type CLEAR_FORM', () => {
-    expect(clearBookForm()).toEqual({
-      type: types.CLEAR_BOOK_FORM,
+describe('addBook', () => {
+  it('returns an action of type ADD_BOOK', () => {
+    expect(actions.addBook().type).toBe(types.ADD_BOOK);
+  });
+
+  describe('when title is defined', () => {
+    const title = 'Title';
+
+    it('returns an action containing that title', () => {
+      expect(actions.addBook(title).title).toBe(title);
+    });
+  });
+
+  describe('when author is defined', () => {
+    const author = 'Author';
+
+    it('returns an action containing that author', () => {
+      expect(actions.addBook(null, author).author).toBe(author);
+    });
+  });
+});
+
+describe('titleChange', () => {
+  it('returns an action of type TITLE_CHANGE', () => {
+    expect(actions.titleChange().type).toBe(types.TITLE_CHANGE);
+  });
+
+  describe('when title is defined', () => {
+    const title = 'Title';
+
+    it('returns an action containing that title', () => {
+      expect(actions.titleChange(title).title).toBe(title);
+    });
+  });
+});
+
+describe('authorChange', () => {
+  it('returns an action of type AUTHOR_CHANGE', () => {
+    expect(actions.authorChange().type).toBe(types.AUTHOR_CHANGE);
+  });
+
+  describe('when author is defined', () => {
+    const author = 'Author';
+
+    it('returns an action containing that author', () => {
+      expect(actions.authorChange(author).author).toBe(author);
     });
   });
 });
--- app/constants/index.js
+++ app/constants/index.js
@@ -1,4 +1,3 @@
-export const CLEAR_BOOK_FORM = 'CLEAR_BOOK_FORM';
 export const ADD_BOOK = 'ADD_BOOK';
 export const TITLE_CHANGE = 'TITLE_CHANGE';
 export const AUTHOR_CHANGE = 'AUTHOR_CHANGE';
