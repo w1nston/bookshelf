22b9816ecb8a6b76e6d2844a1a73801ddba876ba
w1nston <wallander.jonas@gmail.com>
2016-06-08 21:58:54 +0200

    Add test for onChange on <TextInput />.

--- app/components/__tests__/TextInput.spec.js
+++ app/components/__tests__/TextInput.spec.js
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react';
-import expect from 'expect';
+import expect, { createSpy } from 'expect';
 import { shallow } from 'enzyme';
 import TextInput from '../TextInput';
 
@@ -77,6 +77,20 @@ describe('TextInput', () => {
   });
 
   describe('onChange', () => {
-    // TODO
+    it('is set in propTypes declaration', () => {
+      expect(TextInput.propTypes.onChange).toBe(PropTypes.func);
+    });
+
+    describe('when defined', () => {
+      const onChange = createSpy();
+
+      it('triggers the provided callback', () => {
+        const newValue = 'New value';
+        const component = shallow(<TextInput onChange={onChange} />);
+        const event = { target: { value: newValue }};
+        component.find('input').simulate('change', event);
+        expect(onChange).toHaveBeenCalledWith(event);
+      });
+    });
   });
 });
