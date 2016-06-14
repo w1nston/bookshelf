3cd1010fa638fef6d5dde7eb05bb413fe842e4b1
w1nston <wallander.jonas@gmail.com>
2016-06-14 20:47:08 +0200

    Refactor BookForm.spec.js to not import React.PropTypes.

--- app/containers/__tests__/BookForm.spec.js
+++ app/containers/__tests__/BookForm.spec.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import expect, { createSpy } from 'expect';
 import { shallowWithStore } from '../../../utils/redux-spec-utils';
 import BookFormContainer from '../BookForm';
