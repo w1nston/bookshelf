649f0044c7c2f82afd55a81cc4154950237718ec
w1nston <wallander.jonas@gmail.com>
2016-07-25 17:11:40 +0200

    Add connection to bookshelf-server api, fetching real books. Remove mock books.

--- app/services/index.js
+++ app/services/index.js
@@ -1,11 +1,23 @@
-import mockBooks from './mockBooks';
-
-const FAKE_REQUEST_TIMEOUT = 1000;
+const apiBaseURL = 'http://localhost:1337';
 
 export const bookshelfApi = {
   getBooks() {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(mockBooks), FAKE_REQUEST_TIMEOUT);
+    return new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest();
+      request.open('GET', `${apiBaseURL}/books`);
+      request.onload = function onload() {
+        if (request.status === 200) {
+          resolve(JSON.parse(request.response));
+        } else {
+          reject(new Error(request.statusText));
+        }
+      };
+      request.onerror = function onerror() {
+        reject(
+          new Error('Network error attempting to request books from bookshelf-server api.')
+        );
+      };
+      request.send();
     });
   },
 };
--- app/services/mockBooks.js
+++
@@ -1,4 +0,0 @@
-export default [
-  { id: 1, title: 'Clean Code', author: 'Robert C. Martin' },
-  { id: 2, title: 'TDD by Example', author: 'Kent Beck' },
-];
