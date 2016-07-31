cad3cd7fdcae762090f465a7067543a450e0e44f
w1nston <wallander.jonas@gmail.com>
2016-07-31 10:37:17 +0200

    Add tests for creating a book in bookSaga.

--- app/sagas/__tests__/bookSaga.spec.js
+++ app/sagas/__tests__/bookSaga.spec.js
@@ -2,16 +2,44 @@ import 'babel-polyfill';
 import expect from 'expect';
 import { takeEvery } from 'redux-saga';
 import { call, put, fork } from 'redux-saga/effects';
-import booksSaga, { fetchBooks, watchFetchBooks } from '../booksSaga';
+import booksSaga, {
+  createBook,
+  fetchBooks,
+  watchCreateBook,
+  watchFetchBooks,
+} from '../booksSaga';
 import { bookshelfApi } from '../../services';
 import * as actions from '../../actions/booksActions';
-import { FETCH_ALL_BOOKS } from '../../constants';
+import {
+  ADD_BOOK,
+  FETCH_ALL_BOOKS,
+} from '../../constants';
 
 describe('book sagas', () => {
+  describe('*createBooks', () => {
+    describe('with correct parameters', () => {
+      const action = { type: ADD_BOOK, title: 'title', author: 'author' };
+      const generator = createBook(action);
+
+      it('yields a call to bookshelfApi.createBook', () => {
+        const next = generator.next();
+        expect(next.value).toEqual(
+          call(bookshelfApi.createBook, action.title, action.author)
+        );
+      });
+
+      it('yields a put actions.createdBook', () => {
+        const success = true;
+        const next = generator.next(success);
+        expect(next.value).toEqual(put(actions.createdBook(success)));
+      });
+    });
+  });
+
   describe('*fetchBooks', () => {
     const generator = fetchBooks();
 
-    it('yields bookshelfApi.getBooks', () => {
+    it('yields a call to bookshelfApi.getBooks', () => {
       const next = generator.next();
       expect(next.value).toEqual(call(bookshelfApi.getBooks));
     });
@@ -21,17 +49,27 @@ describe('book sagas', () => {
         { id: 1, title: 'title', author: 'author' },
       ];
 
-      it('yields actions.receiveBooks', () => {
+      it('yields a put actions.receiveBooks', () => {
         const next = generator.next(books);
         expect(next.value).toEqual(put(actions.receiveBooks(books)));
       });
     });
   });
 
+  describe('*watchCreateBook', () => {
+    const generator = watchCreateBook();
+
+    it('yields a call to createBook on action ADD_BOOK', () => {
+      const next = generator.next();
+      expect(next.value)
+        .toEqual(call(takeEvery, ADD_BOOK, createBook));
+    });
+  });
+
   describe('*watchFetchBooks', () => {
     const generator = watchFetchBooks();
 
-    it('yields fetchBooks', () => {
+    it('yields a call to fetchBooks on action FETCH_ALL_BOOKS', () => {
       const next = generator.next();
       expect(next.value)
         .toEqual(call(takeEvery, FETCH_ALL_BOOKS, fetchBooks));
@@ -43,7 +81,10 @@ describe('book sagas', () => {
 
     it('yields a fork of watchFetchBooks', () => {
       const next = generator.next();
-      expect(next.value).toEqual([fork(watchFetchBooks)]);
+      expect(next.value).toEqual([
+        fork(watchFetchBooks),
+        fork(watchCreateBook),
+      ]);
     });
   });
 });
--- app/sagas/booksSaga.js
+++ app/sagas/booksSaga.js
@@ -2,11 +2,20 @@ import { takeEvery } from 'redux-saga';
 import { put, call, fork } from 'redux-saga/effects';
 import * as actions from '../actions/booksActions';
 import { bookshelfApi } from '../services';
-import { FETCH_ALL_BOOKS } from '../constants';
+import { FETCH_ALL_BOOKS, ADD_BOOK } from '../constants';
 
 export function *fetchBooks() {
   const books = yield call(bookshelfApi.getBooks);
   yield put(actions.receiveBooks(books));
+}
+
+export function *createBook(action) {
+  const success = yield call(bookshelfApi.createBook, action.title, action.author);
+  yield put(actions.createdBook(success));
+}
+
+export function *watchCreateBook() {
+  yield call(takeEvery, ADD_BOOK, createBook);
 }
 
 export function *watchFetchBooks() {
@@ -16,5 +25,6 @@ export function *watchFetchBooks() {
 export default function *booksSaga() {
   yield [
     fork(watchFetchBooks),
+    fork(watchCreateBook),
   ];
 }
